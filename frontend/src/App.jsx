48c307c718bd1d42cd0f07dad1be796336185011
Arsdev998 <arsdev998@gmail.com>
2024-04-03 23:27:56 +0700

    header and explore menu

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Cart from "./pages/Cart/Cart";
+import PlaceOlder from "./pages/PlaceOlder/PlaceOlder";
 const App = () => {
   return (
     <div className="app">
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/order" element={<PlaceOlder/>} />
+      </Routes>
     </div>
   );
 };
---
+++ frontend/src/pages/PlaceOlder/PlaceOlder.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./PlaceOlder.css";
+const Placeholder = () => {
+  return <div></div>;
+};
+
+export default Placeholder;
